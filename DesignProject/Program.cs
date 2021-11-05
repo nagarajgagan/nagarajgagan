48e331f7959df5729b11f1e8afe55fbb892db64b
nagarajgagan <nagarajgagan777@gmail.com>
2021-11-05 14:30:54 +0530

    Initial commit - project creation.

---
+++ DesignProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
